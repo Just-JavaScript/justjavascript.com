9ab5f1f058f988baaf0382d798f29f92c73eef2e
Vojta Holik <vojta@egghead.io>
2021-02-18 18:06:43 +0100

    add "Counting the Values Part 1" quiz

--- data/quizzes/index.js
+++ data/quizzes/index.js
@@ -1,5 +1,11 @@
 import ValuesAndVariables from './values-and-variables.json'
 import TheJavaScriptUniverse from './the-javascript-universe.json'
+import CountingTheValuesPart1 from './counting-the-values-pt-1.json'
 import Demo from './demo.json'
 
-export default [Demo, ValuesAndVariables, TheJavaScriptUniverse]
+export default [
+  Demo,
+  ValuesAndVariables,
+  TheJavaScriptUniverse,
+  CountingTheValuesPart1,
+]
