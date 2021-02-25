18c4ce14ad9ed56c81c5d131274f84cd6547251d
Vojta Holik <vojta@egghead.io>
2021-02-25 18:12:18 +0100

    convert gifs to videos

--- components/mdx/video.js
+++ components/mdx/video.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 const Video = (props) => {
   return (
-    <video
-      controls
-      loop
-      muted
-      playsInline
-      src={props.src}
-      autoPlay
-      className="border rounded-lg border-gray-100 shadow-lg hover:shadow-xl cursor-pointer transition-all duration-300 ease-in-out"
-      {...props}
-    />
+    <div className="sm:mx-0 -mx-5">
+      <video
+        controls
+        loop
+        muted
+        playsInline
+        src={props.src}
+        autoPlay
+        className="sm:mx-0 sm:border sm:rounded-lg border-gray-100 sm:shadow-lg hover:shadow-xl cursor-pointer transition-all duration-300 ease-in-out"
+        {...props}
+      />
+    </div>
   )
 }
 
