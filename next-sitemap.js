8c868dfc3aefcf5d584d1dc0bd6ce55c75b861b9
Vojta Holik <vojta@egghead.io>
2021-03-19 10:14:20 +0100

    add sitemap and dont forget to exclude pages

---
+++ next-sitemap.js
@@ -0,0 +1,18 @@
+require('dotenv-flow').config()
+
+module.exports = {
+  siteUrl: `https://justjavascript.com`,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  exclude: [
+    '/buy',
+    '/content',
+    '/login',
+    '/confirm',
+    '/confirmed',
+    '/excited',
+    '/unsubscribed',
+  ],
+}
