b65b2c5d927ff7f21c9fd6a060c9299c28d210d9
Dan Abramov <dan.abramov@gmail.com>
2021-05-31 18:19:37 +0100

    Rewrite the Excalidraw integration (#63)
    * Rewrite Excalidraw integration
    
    * Fix focus jump
    
    * Remove syncing
    
    * Remove nested scroll
    
    * Add explicit reset action
    
    * Fix a crash

--- components/mdx/sketch.js
+++ components/mdx/sketch.js
@@ -1,5 +1,5 @@
-import { createRef, useRef, useEffect, memo } from 'react';
-import Excalidraw from '../excalidraw/excalidraw-iframe'
+import { createRef, useMemo, useRef, useState, useEffect, memo } from 'react';
+import Excalidraw from '../excalidraw/excalidraw'
 import debounce from 'lodash/debounce'
 import {ErrorBoundary} from 'react-error-boundary'
 
@@ -50,32 +50,51 @@ function ErrorFallback({error, resetErrorBoundary}) {
   )
 }
 
-function Sketch({ id }) {
+function Sketch({ id, baseId }) {
   if (!id) {
     throw Error('Specify an id for persistence');
   }
   // Bump if Excalidraw changes the format.
   const key = 'excalidraw::v1::' + id;
-  const ref = useRef(null);
-  if (!ref.current) {
-    ref.current = new ExcalidrawImperative(key);
-  }
-  useEffect(() => {
-    const xc = ref.current;
-    xc.listen();
-    return () => xc.unlisten();
-  }, []);
+  const initialElements = useMemo(() => readState(key), [key]);
+  const ref = useRef();
   return (
-    <Excalidraw
-      ref={ref.current.xcRef}
-      initialData={{
-        elements: ref.current.elements,
-        scrollToCenter: true
-      }}
-      onChange={ref.current.handleChange}
-    />
+    <>
+      {baseId && (
+        <div style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <button
+            className="mb-8 mt-8 py-2 px-8 leading-6 text-white transition-colors duration-200 ease-in-out bg-black rounded-md sm:px-10 sm:py-3 hover:bg-gray-900"
+            onClick={() => {
+              ref.current.updateScene({
+                elements: JSON.parse(JSON.stringify(latestState.get(baseId)))
+              });
+            }}
+          >
+            Load previous sketch
+          </button>
+        </div>
+      )}
+      <Excalidraw
+        ref={ref}
+        initialData={{
+          elements: initialElements,
+          scrollToContent: true
+        }}
+        onChange={(elements) => {
+          latestState.set(id, elements);
+          writeState(key, elements);
+        }}
+      />
+    </>
   );
 }
+
+let latestState = new Map();
 
 function readState(key) {
   try {
@@ -94,57 +113,3 @@ function writeState(key, state) {
 }
 
 writeState = debounce(writeState, 1000);
-
-let listeners = {};
-let isNotifying = false;
-let nextId = 0;
-
-class ExcalidrawImperative {
-  constructor(key) {
-    this.id = nextId++;
-    this.key = key;
-    this.xcRef = createRef();
-    this.elements = readState(key);
-    this.broadcastSyncedEdit = debounce(this.broadcastSyncedEdit, 1000);
-  }
-
-  listen = () => {
-    listeners[this.key] = listeners[this.key] || [];
-    listeners[this.key].push(this.handleSyncedEdit);
-  };
-
-  unlisten = () => {
-    listeners[this.key] = listeners[this.key].filter(l =>
-      l === this.handleSyncedEdit
-    )
-  };
-
-  handleSyncedEdit = (originId, elements) => {
-    if (originId !== this.id && this.xcRef.current) {
-      // Excalidraw gets buggy if we reuse the elements.
-      this.xcRef.current.updateScene(JSON.parse(JSON.stringify({
-        elements,
-      })));
-    }
-  };
-
-  handleChange = (elements) => {
-    if (!isNotifying) {
-      this.elements = elements;
-      writeState(this.key, elements);
-      this.broadcastSyncedEdit(elements);
-    }
-  };
-
-  broadcastSyncedEdit = (elements) => {
-    const toNotify = listeners[this.key];
-    if (toNotify) {
-      isNotifying = true;
-      try {
-        toNotify.forEach(cb => cb(this.id, elements));
-      } finally {
-        isNotifying = false;
-      }
-    }
-  }
-}
