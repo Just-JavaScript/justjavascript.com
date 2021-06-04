7821f69a9607829f9f20595cf6fb6bb806717ec3
Vojta Holik <vojta@egghead.io>
2021-06-04 15:08:02 +0200

    add loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Router from 'next/router'
 import {MDXProvider} from '@mdx-js/react'
 import 'focus-visible'
 import {ViewerProvider} from 'context/viewer-context'
@@ -6,6 +7,11 @@ import {ProgressProvider} from 'context/progress-context'
 import mdxComponents from '../components/mdx'
 import '../styles/index.css'
 import '../styles/gifplayer.css'
+import NProgress from 'nprogress'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const App = ({Component, pageProps}) => {
   return (
