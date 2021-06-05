8bc35288bfab5622756d2965da8f0535a8cb8380
Vojta Holik <vojta@egghead.io>
2021-06-05 11:04:18 +0200

    lazy load

--- components/mdx/gif-player.js
+++ components/mdx/gif-player.js
@@ -13,6 +13,12 @@ export default function GifPlayer(props) {
       className="transition-all duration-300 ease-in-out border border-gray-100 rounded-lg shadow-lg cursor-pointer hover:shadow-xl"
     />
   ) : (
-    <img src={props.src} alt={props.alt} className="mx-auto" {...props} />
+    <img
+      src={props.src}
+      alt={props.alt}
+      loading="lazy"
+      className="mx-auto"
+      {...props}
+    />
   )
 }
