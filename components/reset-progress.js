427f6d3f892230eeb07430898af108ed98a79e94
Vojta Holik <vojta@egghead.io>
2021-06-05 08:46:08 +0200

    feature: ability to reset user progress (#65)
    * feature: ability to clean user progress
    
    * adjust wording
    
    * make nice
    
    * tweak
    
    * only reset submitted answers within a quiz
    
    * clean up import

---
+++ components/reset-progress.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import {
+  AlertDialogLabel,
+  AlertDialogDescription,
+  AlertDialogOverlay,
+  AlertDialogContent,
+} from '@reach/alert-dialog'
+import resetQuizAnswers from 'utils/reset-quiz-answers'
+
+const ResetProgress = (questions) => {
+  function handleResetProgress() {
+    resetQuizAnswers(questions)
+    close()
+  }
+  const [showDialog, setShowDialog] = React.useState(false)
+  const cancelRef = React.useRef()
+  const open = () => setShowDialog(true)
+  const close = () => setShowDialog(false)
+
+  return (
+    <div>
+      <button
+        onClick={open}
+        className="flex items-center px-4 py-2 transition-all duration-200 ease-in-out transform bg-white rounded-lg shadow-xl sm:text-lg hover:scale-105"
+      >
+        <i className="gg-undo" />
+        <span className="pl-3">Start over</span>
+      </button>
+      {showDialog && (
+        <AlertDialogOverlay
+          className="z-50 bg-black backdrop-filter backdrop-blur-lg bg-opacity-20"
+          leastDestructiveRef={cancelRef}
+          onDismiss={close}
+        >
+          <AlertDialogContent className="rounded-lg shadow-xl">
+            <AlertDialogLabel className="text-xl font-semibold">
+              Please Confirm
+            </AlertDialogLabel>
+            <AlertDialogDescription className="pt-1 pb-6 opacity-80">
+              Are you sure you want to start this quiz over and reset your
+              progress?
+            </AlertDialogDescription>
+            <div className="flex space-x-5">
+              <button
+                className="px-4 py-2 text-white transition-colors duration-100 ease-in-out bg-black rounded-md hover:bg-gray-600"
+                onClick={handleResetProgress}
+              >
+                Yes, reset my progress
+              </button>{' '}
+              <button
+                className="px-4 py-2 transition-colors duration-100 ease-in-out border border-gray-100 rounded-md hover:bg-gray-100"
+                ref={cancelRef}
+                onClick={close}
+              >
+                Nevermind, don't reset
+              </button>
+            </div>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      )}
+    </div>
+  )
+}
+
+export default ResetProgress
