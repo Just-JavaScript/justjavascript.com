29b2a4633048265ad973d3b57d2f86b220e01f4c
Vojta Holik <vojta@egghead.io>
2021-07-02 16:59:26 +0200

    adjust

--- pages/index.js
+++ pages/index.js
@@ -71,22 +71,26 @@ const LandingPage = ({ bundles }) => {
         </svg>
       </header>
       <div className="-mx-5 -mb-3">
-        <div className="relative flex items-start justify-center h-full ">
+        <div className="relative flex items-start justify-center h-full">
           <div className="z-20 xl:-mt-16 bg-white md:p-16 p-5 sm:shadow-container mx-auto flex items-center jusfify-start">
             <article className="w-full max-w-screen-sm sm:pt-0 pt-8 pb-8 mx-auto prose sm:pb-16 lg:prose-lg sm:prose-lg">
               {sellingLive ? <LandingCopy /> : <SubscribeCopy />}
             </article>
           </div>
-          <Image
-            aria-hidden="true"
-            quality={80}
-            layout="fill"
-            src={bg}
-            alt=""
-            objectFit="cover"
-            loading="eager"
-            className="opacity-30"
-          />
+
+          <div className="h-[37%] absolute w-full left-0 bottom-0">
+            <Image
+              aria-hidden="true"
+              quality={100}
+              layout="fill"
+              src={bg}
+              alt=""
+              objectFit="cover"
+              objectPosition="top"
+              loading="eager"
+              className="opacity-100"
+            />
+          </div>
         </div>
       </div>
       {sellingLive && (
