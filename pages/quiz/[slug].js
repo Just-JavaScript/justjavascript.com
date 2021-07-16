31e2459328424ec3bd7c08f34ec602db36b5c0b9
Vojta Holik <vojta@egghead.io>
2021-07-16 14:24:57 +0200

    refactor to enable serverSideAuthCheck

---
+++ pages/quiz/[slug].js
@@ -0,0 +1,63 @@
+import React from 'react'
+import fs from 'fs'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
+import mdxComponents from 'components/mdx'
+import matter from 'gray-matter'
+import { QUIZ_PATH } from 'utils/mdxUtils'
+import path from 'path'
+import isArray from 'lodash/isArray'
+import useLoginRequired from 'hooks/use-login-required'
+import { useViewer } from 'context/viewer-context'
+import { serverSideAuthCheck } from 'utils/serverSideAuthCheck'
+
+const components = mdxComponents
+
+const EpisodePage = ({ source, meta, ...props }) => {
+  const isVerifyingLogin = useLoginRequired()
+  const { isUnclaimedBulkPurchaser, loading } = useViewer()
+
+  if (isVerifyingLogin || isUnclaimedBulkPurchaser || loading) {
+    return null
+  }
+
+  return <MDXRemote {...source} components={components} />
+}
+
+export const getServerSideProps = async ({ params, req }) => {
+  const possibleAuthRedirect = serverSideAuthCheck({ req })
+
+  if (possibleAuthRedirect) {
+    return possibleAuthRedirect
+  }
+
+  if (!params?.slug || isArray(params?.slug)) {
+    return {
+      notFound: true,
+    }
+  }
+
+  const postFilePath = path.join(QUIZ_PATH, `${params.slug}.mdx`)
+
+  if (!fs.existsSync(postFilePath)) {
+    return {
+      notFound: true,
+    }
+  }
+
+  const source = fs.readFileSync(postFilePath)
+  const { content, data } = matter(source)
+  const mdxSource = await serialize(content, {
+    components,
+    mdxOptions: {},
+    scope: data,
+  })
+
+  return {
+    props: {
+      source: mdxSource,
+    },
+  }
+}
+
+export default EpisodePage
--- utils/mdxUtils.js
+++ utils/mdxUtils.js
@@ -2,7 +2,12 @@ import fs from 'fs'
 import path from 'path'
 
 export const CONTENT_PATH = path.join(process.cwd(), 'content')
+export const QUIZ_PATH = path.join(process.cwd(), 'content/quiz')
 
 export const contentFilePaths = fs
   .readdirSync(CONTENT_PATH)
+  .filter((path) => /\.mdx?$/.test(path))
+
+export const quizFilePaths = fs
+  .readdirSync(QUIZ_PATH)
   .filter((path) => /\.mdx?$/.test(path))
