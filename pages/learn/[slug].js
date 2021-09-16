240c6f148a794c3baa35cab6eb62fb74c5e7c4be
Joel Hooks <joelhooks@gmail.com>
2021-09-16 12:13:10 -0700

    add error logging via honeycomb to SSR

--- pages/learn/[slug].js
+++ pages/learn/[slug].js
@@ -8,6 +8,7 @@ import { CONTENT_PATH } from 'utils/mdxUtils'
 import path from 'path'
 import isArray from 'lodash/isArray'
 import { checkAuth } from 'utils/check-auth'
+import {hny} from "../../utils/configured-libhoney";
 
 const components = mdxComponents
 
@@ -16,39 +17,57 @@ const EpisodePage = ({ source, meta, ...props }) => {
 }
 
 export const getServerSideProps = async ({ params, req }) => {
-  const redirect = await checkAuth(req)
+  const event = hny.newEvent();
 
-  if (redirect) {
-    return redirect
-  }
-
-  // MDX text - can be from a local file, database, anywhere
-  if (!params?.slug || isArray(params?.slug)) {
-    return {
-      notFound: true,
-    }
-  }
-
-  const postFilePath = path.join(CONTENT_PATH, `${params.slug}.mdx`)
-
-  if (!fs.existsSync(postFilePath)) {
-    return {
-      notFound: true,
-    }
-  }
-
-  const source = fs.readFileSync(postFilePath)
-  const { content, data } = matter(source)
-  const mdxSource = await serialize(content, {
-    components,
-    scope: data,
+  event.add({
+    name: getServerSideProps.name,
+    ip: req.ip,
+    path: req.path,
   })
 
-  return {
-    props: {
-      source: mdxSource,
-    },
+  try {
+    const redirect = await checkAuth(req)
+
+    if (redirect) {
+      return redirect
+    }
+
+    // MDX text - can be from a local file, database, anywhere
+    if (!params?.slug || isArray(params?.slug)) {
+      return {
+        notFound: true,
+      }
+    }
+
+    const postFilePath = path.join(CONTENT_PATH, `${params.slug}.mdx`)
+
+    if (!fs.existsSync(postFilePath)) {
+      return {
+        notFound: true,
+      }
+    }
+
+    const source = fs.readFileSync(postFilePath)
+    const { content, data } = matter(source)
+    const mdxSource = await serialize(content, {
+      components,
+      scope: data,
+    })
+
+    return {
+      props: {
+        source: mdxSource,
+      },
+    }
+  } catch (error) {
+    console.error(error)
+    event.add({error})
+    return res.status(500).json({error: 'Unexpected error.'})
+  } finally {
+    event.send()
   }
+
+
 }
 
 export default EpisodePage
