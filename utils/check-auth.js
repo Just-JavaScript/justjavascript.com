57b4c506a766c59f6ea778e24bf38c7be3df6194
Joel Hooks <joelhooks@gmail.com>
2021-09-16 12:21:28 -0700

    try/catch in checkAuth to log error

--- utils/check-auth.js
+++ utils/check-auth.js
@@ -2,34 +2,52 @@ import { purchaseVerifier } from 'utils/egghead-purchase-verifier'
 import { serverSideAuthCheck } from 'utils/serverSideAuthCheck'
 import { ACCESS_TOKEN_KEY } from './auth'
 import get from 'lodash/get'
+import {hny} from "./configured-libhoney";
 
 export const checkAuth = async (req) => {
-  const possibleAuthRedirect = await serverSideAuthCheck({ req })
+  const event = hny.newEvent();
 
-  if (possibleAuthRedirect) {
-    return possibleAuthRedirect
-  }
+  event.add({
+    name: checkAuth.name,
+    ip: req.ip,
+    path: req.path,
+  })
 
-  const authToken = get(req.cookies, ACCESS_TOKEN_KEY)
-  const { isUnclaimedBulkPurchaser, canViewContent } = await purchaseVerifier(
-    authToken
-  )
-  // check if they need to claim a seat
-  if (isUnclaimedBulkPurchaser) {
-    return {
-      redirect: {
-        destination: '/invoice',
-        permanent: false,
-      },
+  try {
+    const possibleAuthRedirect = await serverSideAuthCheck({ req })
+
+    if (possibleAuthRedirect) {
+      return possibleAuthRedirect
     }
+
+    const authToken = get(req.cookies, ACCESS_TOKEN_KEY)
+    const { isUnclaimedBulkPurchaser, canViewContent } = await purchaseVerifier(
+        authToken
+    )
+    // check if they need to claim a seat
+    if (isUnclaimedBulkPurchaser) {
+      return {
+        redirect: {
+          destination: '/invoice',
+          permanent: false,
+        },
+      }
+    }
+
+    if (!canViewContent) {
+      return {
+        redirect: {
+          destination: '/buy',
+          permanent: false,
+        },
+      }
+    }
+  } catch(error) {
+    console.error(error)
+    event.add({error})
+    throw(error)
+  } finally {
+    event.send()
   }
 
-  if (!canViewContent) {
-    return {
-      redirect: {
-        destination: '/buy',
-        permanent: false,
-      },
-    }
-  }
 }
